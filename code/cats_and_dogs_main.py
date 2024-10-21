433fe7da2f19507f910f3c62f304735fd3621b2e
Willow Hart <willowhart151@gmail.com>
2024-10-21 14:36:36 +0100

    added plot of validation and accuracy to enble fine tuning

--- code/cats_and_dogs_main.py
+++ code/cats_and_dogs_main.py
@@ -52,9 +52,34 @@ model.compile(
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy'])
 
+epochs = 10
+
 history = model.fit(
     tf_ds["train"],
     validation_data = tf_ds['validation'],
-    epochs = 4)
+    epochs = epochs)
 
 print(model.summary())
+
+## Visualising training results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1,2,2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+
+plt.savefig(fname='temp/TrainingAndValidation')
